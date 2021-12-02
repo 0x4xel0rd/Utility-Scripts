ad620126d28c7f9d48290804b98dc281243c1707
Suvam Barui <67534409+Shuvsec@users.noreply.github.com>
2021-12-02 11:42:46 -0500

    Created Script for installing pip for Python2
    Though deprecated , so many libraries so still used in some applications , keeping it handy.

---
+++ pipforpython2.sh
@@ -0,0 +1,4 @@
+sudo apt install -y python2 python2.7-dev libpython2-dev
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+sudo python2 -m pip install -U setuptools wheel
