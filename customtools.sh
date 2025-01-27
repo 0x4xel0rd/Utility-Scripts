81c2344cc7b1360a743f70031940be3af8f2afe7
Suvam Barui <67534409+0x4xel0rd@users.noreply.github.com>
2025-01-27 12:20:20 -0500

    Update customtools.sh

--- customtools.sh
+++ customtools.sh
@@ -30,6 +30,8 @@ sudo apt-get install finalrecon
 #useful github repositiories
 #Penelope is a shell handler designed to be easy to use and intended to replace netcat when exploiting RCE vulnerabilities. It is compatible with Linux and macOS and requires Python 3.6 or higher. It is a standalone script using only Python's native library, and it is intended to remain this way.
 git clone https://github.com/brightio/penelope.git
+#PEASS privesc
+wget https://github.com/peass-ng/PEASS-ng/releases/download/20250126-41ed0f6a/linpeas.sh
 
 #port-forwarding tools
 #chisel
@@ -47,4 +49,6 @@ wget https://github.com/nicocha30/ligolo-ng/releases/download/v0.7.5/ligolo-ng_p
 
 #A tool to change grafana hash to hashcat crackable format
 git clone https://github.com/iamaldi/grafana2hashcat.git # save hash in this format 'hash, salt' > hash.txt | hashcat -m 10900 -a 0 bigbang.grafanahashcat.hashes /opt/Passwords/rockyou.txt
+
+
 
