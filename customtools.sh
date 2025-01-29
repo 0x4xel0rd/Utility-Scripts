bb881120fb7b6f82c4e82d792682882955d310d1
Suvam Barui <67534409+0x4xel0rd@users.noreply.github.com>
2025-01-29 11:34:58 -0500

    Update customtools.sh

--- customtools.sh
+++ customtools.sh
@@ -29,6 +29,9 @@ sudo apt-get install finalrecon
 
 #useful github repositiories
 #Penelope is a shell handler designed to be easy to use and intended to replace netcat when exploiting RCE vulnerabilities. It is compatible with Linux and macOS and requires Python 3.6 or higher. It is a standalone script using only Python's native library, and it is intended to remain this way.
+mkdir /opt/privesc
+cd /opt/privesc
+
 git clone https://github.com/brightio/penelope.git
 #PEASS privesc
 wget https://github.com/peass-ng/PEASS-ng/releases/download/20250126-41ed0f6a/linpeas.sh
@@ -49,6 +52,22 @@ wget https://github.com/nicocha30/ligolo-ng/releases/download/v0.7.5/ligolo-ng_p
 
 #A tool to change grafana hash to hashcat crackable format
 git clone https://github.com/iamaldi/grafana2hashcat.git # save hash in this format 'hash, salt' > hash.txt | hashcat -m 10900 -a 0 bigbang.grafanahashcat.hashes /opt/Passwords/rockyou.txt
+
+#Windows Binaries and Active Directory Tools
+mkdir /opt/windows/AD
+cd /opt/windows/AD
+
+curl https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_linux_amd64 -o kerbrute_linux_amd64
+curl https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_windows_amd64.exe -o kerbrute_windows_amd64.exe
+
+git clone https://github.com/Flangvik/SharpCollection.git
+
+#Installing neo4j for graph support and bloodhound
+sudo apt-get install neo4j
+sudo apt-get install bloodhound
+
+#Installinf impacket tools
+sudo apt-get install python3-impacket
 
 
 
